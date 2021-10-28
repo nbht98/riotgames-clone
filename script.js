dd1cfdedd057fcc0a07e80fe2e9216336ccb7543
Ngo Ba Hoang Thien <nbht98@gmail.com>
2021-10-28 16:58:41 +0700

    add effect, hover, and left navigation

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ const scrollToTopBtn = document.querySelector(".to-surface");
 const rootElement = document.documentElement;
 
 const navLeftTogglerOpen = document.querySelector(".navbar__left-toggler-open");
-const navLeftTogglerClose = document.querySelector(".navbar__left-toggler-close");
+const navLeftTogglerClose = document.querySelector(
+  ".navbar__left-toggler-close"
+);
 const navLeftCollapse = document.querySelector(".navbar__left-collapse");
-const body = document.querySelector("body");
+const whoWeAre = document.querySelector(".who-we-are");
+const whoWeAreSub = document.querySelector(".who-we-are-sub");
+const aboutUs = document.querySelector(".about-us");
+const aboutUsSub = document.querySelector(".about-us-sub");
+const whoWeAreCloseButton = document.querySelector(".sub-close-button-1");
+const aboutUsCloseButton = document.querySelector(".sub-close-button-2");
 
+const navbarSubCollapse = document.querySelector(".navbar-subcollapse");
+const navSubTogglerClose1 = document.querySelector(
+  ".navbar-subtoggler-close-1"
+);
+const navSubTogglerClose2 = document.querySelector(
+  ".navbar-subtoggler-close-2"
+);
+
+const body = document.querySelector("body");
+const nav = document.querySelector(".navbar");
 
 navTogglerOpen.addEventListener("click", () => {
   navCollapse.classList.add("show-navbar");
@@ -21,8 +38,39 @@ navTogglerClose.addEventListener("click", () => {
   search.classList.remove("show-search");
 });
 
+navSubTogglerClose1.addEventListener("click", () => {
+  navCollapse.classList.remove("show-navbar");
+  whoWeAreSub.classList.remove("show-navbar");
+  aboutUsSub.classList.remove("show-navbar");
+  search.classList.remove("show-search");
+});
+navSubTogglerClose2.addEventListener("click", () => {
+  navCollapse.classList.remove("show-navbar");
+  whoWeAreSub.classList.remove("show-navbar");
+  aboutUsSub.classList.remove("show-navbar");
+  search.classList.remove("show-search");
+});
+
+whoWeAre.addEventListener("click", () => {
+  whoWeAreSub.classList.add("show-navbar");
+});
+
+aboutUs.addEventListener("click", () => {
+  aboutUsSub.classList.add("show-navbar");
+});
+
+whoWeAreCloseButton.addEventListener("click", () => {
+  whoWeAreSub.classList.remove("show-navbar");
+
+});
+aboutUsCloseButton.addEventListener("click", () => {
+  aboutUsSub.classList.remove("show-navbar");
+});
+
+
+
 scrollToTopBtn.addEventListener("click", () => {
-  console.log(1)
+  console.log(1);
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth",
@@ -31,15 +79,20 @@ scrollToTopBtn.addEventListener("click", () => {
 
 navLeftTogglerOpen.addEventListener("click", () => {
   navLeftCollapse.classList.add("show-navbar__left");
-  body.classList.add("hide-scroll")
+  body.classList.add("hide-scroll");
 });
 
 navLeftTogglerClose.addEventListener("click", () => {
   navLeftCollapse.classList.remove("show-navbar__left");
-  body.classList.remove("hide-scroll")
+  body.classList.remove("hide-scroll");
 });
 
-
-// window.addEventListener('scroll', () => {
-//   console.log(this)
-// })
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    nav.classList.add("navbar-color");
+    search.classList.add("search-color");
+  } else {
+    nav.classList.remove("navbar-color");
+    search.classList.remove("search-color");
+  }
+});
